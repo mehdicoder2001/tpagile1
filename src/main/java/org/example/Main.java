88efa12ec7bb276a54f6c20fa44af7e0fbe38940
mehdicoder2001 <mehdimoussadik08@gmail.com>
2023-10-28 17:16:03 +0100

    mofify

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,7 +8,6 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Entrée with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-     Person person=new Person("Joey","Doe", LocalDate.parse("2013-01-12"));
-     System.out.println(person.getDisplayName()+":"+person.getAge()+"years");
+
     }
 }
---
+++ src/main/java/org/example/Mehdi.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Mehdi {
+
+    private final String nom;
+
+    public Mehdi(String nom, String prenom) {
+        this.nom = nom;
+        this.prenom = prenom;
+    }
+
+    private final String prenom;
+
+}
